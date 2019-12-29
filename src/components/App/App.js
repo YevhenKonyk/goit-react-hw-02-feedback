7ca7166859e91c0cc5a0a0b2f79f69786fb5619f
Yevhen Konyk <yevhen.konyk@gmail.com>
2019-12-29 16:56:35 +0200

    Added basic functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,59 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   state = {
-    message: 'Feedback',
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGoodBtnClick = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  handleNeutralBtnClick = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  handleBadBtnClick = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
   };
 
   render() {
-    const { message } = this.state;
+    const { good, neutral, bad } = this.state;
 
-    return <h1>{message}</h1>;
+    return (
+      <section className="">
+        <h1 className="">Please Leave feedback</h1>
+
+        <button className="" type="button" onClick={this.handleGoodBtnClick}>
+          Good
+        </button>
+        <button className="" type="button" onClick={this.handleNeutralBtnClick}>
+          Neutral
+        </button>
+        <button className="" type="button" onClick={this.handleBadBtnClick}>
+          Bad
+        </button>
+
+        <h2 className="">Statistics</h2>
+        <ul className="">
+          <li className="">
+            <p>Good: {good}</p>
+          </li>
+          <li className="">
+            <p>Neutral: {neutral}</p>
+          </li>
+          <li className="">
+            <p>Bad: {bad}</p>
+          </li>
+        </ul>
+      </section>
+    );
   }
 }
