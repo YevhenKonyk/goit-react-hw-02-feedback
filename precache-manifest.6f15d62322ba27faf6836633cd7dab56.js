f8742ddc5088e821cc2ae9aa2aaa74cfb10a639e
Yevhen Konyk <yevhen.konyk@gmail.com>
2019-12-30 21:40:28 +0200

    Updates

---
+++ precache-manifest.6f15d62322ba27faf6836633cd7dab56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41497f6313a40415c545ea7a58a4b740",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "8160ce8657706172424b",
+    "url": "/goit-react-hw-02-feedback/static/js/2.7e84429f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.7e84429f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "703ad34790663aa87211",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f45e678b.chunk.js"
+  },
+  {
+    "revision": "6d4883b48959809198b2",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f22f9b33.js"
+  }
+]);
